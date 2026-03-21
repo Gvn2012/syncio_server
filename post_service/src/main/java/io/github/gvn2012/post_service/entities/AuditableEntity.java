d9405dd2c66e98dd8cd600c07187361397b7862b
Gavin Ph <vietpham06262@gmail.com>
2026-03-21 14:52:08 +0700

    Init Post Service and Reconstruct User Service

---
+++ post_service/src/main/java/io/github/gvn2012/post_service/entities/AuditableEntity.java
@@ -0,0 +1,24 @@
+package io.github.gvn2012.post_service.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class AuditableEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
--- post_service/src/main/java/io/github/gvn2012/post_service/entities/Post.java
+++ post_service/src/main/java/io/github/gvn2012/post_service/entities/Post.java
@@ -1,11 +1,23 @@
 package io.github.gvn2012.post_service.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
 
 
 @AllArgsConstructor
-@RequiredArgsConstructor
-public class Post {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post extends AuditableEntity{
+
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    @Column(name = "id", nullable = false, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID id;
 }
