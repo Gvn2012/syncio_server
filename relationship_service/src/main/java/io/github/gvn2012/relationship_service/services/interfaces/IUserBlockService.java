16a6f94245e5a3f36d675bb51fc13733a0de5c7c
Gavin Ph <vietpham06262@gmail.com>
2026-04-02 20:03:54 +0700

    feat: implement block status check, add friend request validation, and add Kubernetes infrastructure configurations

--- relationship_service/src/main/java/io/github/gvn2012/relationship_service/services/interfaces/IUserBlockService.java
+++ relationship_service/src/main/java/io/github/gvn2012/relationship_service/services/interfaces/IUserBlockService.java
@@ -1,10 +1,14 @@
 package io.github.gvn2012.relationship_service.services.interfaces;
 
 import io.github.gvn2012.relationship_service.dtos.APIResource;
+import io.github.gvn2012.relationship_service.dtos.responses.CheckBlockStatusResponse;
 import io.github.gvn2012.relationship_service.entities.enums.BlockReason;
 import java.util.UUID;
 
 public interface IUserBlockService {
     APIResource<Void> blockUser(UUID blockerId, UUID blockedId, BlockReason reason, String notes);
+
     APIResource<Void> unblockUser(UUID blockerId, UUID blockedId);
+
+    CheckBlockStatusResponse checkBlockStatus(UUID userIdF, UUID userIdS);
 }
