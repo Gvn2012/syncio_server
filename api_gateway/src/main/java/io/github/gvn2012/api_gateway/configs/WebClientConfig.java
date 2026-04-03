e340594c33188c8d51e6b6edce3268b535d38d00
Gavin Ph <vietpham06262@gmail.com>
2026-04-03 11:07:27 +0700

    feat: add stateless security configurations to user and post services and configure Zipkin WebClient in API gateway

--- api_gateway/src/main/java/io/github/gvn2012/api_gateway/configs/WebClientConfig.java
+++ api_gateway/src/main/java/io/github/gvn2012/api_gateway/configs/WebClientConfig.java
@@ -1,9 +1,13 @@
 package io.github.gvn2012.api_gateway.configs;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
@@ -11,5 +15,14 @@ public class WebClientConfig {
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean(name = "zipkinWebClientBuilder")
+    public WebClient.Builder zipkinWebClientBuilder() {
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(
+                HttpClient.create(ConnectionProvider.builder("zipkin-connection-provider")
+                        .maxIdleTime(Duration.ofSeconds(30))
+                        .build())
+        ));
     }
 }
---
+++ post_service/src/main/java/io/github/gvn2012/post_service/configs/SecurityConfig.java
@@ -0,0 +1,27 @@
+package io.github.gvn2012.post_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .build();
+    }
+}
