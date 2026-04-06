173eaddbcb4dc1f08b6378ebb8e790a4ded50f66
Gavin Ph <vietpham06262@gmail.com>
2026-04-06 17:13:01 +0700

    fix: request cloud-platform scope for GCS client to fix 403 signing error

--- uploading_service/src/main/java/io/github/gvn2012/image_uploading_service/services/impls/GCSServiceImpl.java
+++ uploading_service/src/main/java/io/github/gvn2012/image_uploading_service/services/impls/GCSServiceImpl.java
@@ -1,12 +1,16 @@
 package io.github.gvn2012.image_uploading_service.services.impls;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.*;
 import io.github.gvn2012.image_uploading_service.services.interfaces.GCSServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import jakarta.annotation.PostConstruct;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -16,7 +20,14 @@ public class GCSServiceImpl implements GCSServiceInterface {
     @Value("${gcp.storage.bucket}")
     private String bucket;
 
-    private final Storage storage = StorageOptions.getDefaultInstance().getService();
+    private Storage storage;
+
+    @PostConstruct
+    public void init() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+                .createScoped(Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
+        this.storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    }
 
     @Override
     public URL generateUploadUrl(String objectName, String contentType) {
