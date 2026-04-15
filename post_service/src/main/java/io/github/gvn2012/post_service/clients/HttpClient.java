fa1bc87f8a0044f0683c8de4fae604c60ef32a6d
Gavin Ph <vietpham06262@gmail.com>
2026-04-15 18:30:05 +0700

    refactor: remove ReactiveCircuitBreaker from HttpClient requests

--- post_service/src/main/java/io/github/gvn2012/post_service/clients/HttpClient.java
+++ post_service/src/main/java/io/github/gvn2012/post_service/clients/HttpClient.java
@@ -22,40 +22,34 @@ public abstract class HttpClient {
     protected final String baseUrl;
 
     protected <T> Mono<T> get(@NonNull String uri, @NonNull Class<T> responseType) {
-        ReactiveCircuitBreaker rcb = cbFactory.create("syncio-circuitbreaker");
         return buildClient().get()
                 .uri(uri)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
-                .bodyToMono(responseType)
-                .transform(it -> rcb.run(it));
+                .bodyToMono(responseType);
     }
 
     protected <T> Mono<T> get(
             @NonNull String uri,
             @NonNull ParameterizedTypeReference<T> responseType,
             @NonNull Object... uriVariables) {
-        ReactiveCircuitBreaker rcb = cbFactory.create("syncio-circuitbreaker");
         return buildClient().get()
                 .uri(uri, uriVariables)
                 .accept(Objects.requireNonNull(MediaType.APPLICATION_JSON))
                 .retrieve()
-                .bodyToMono(responseType)
-                .transform(it -> rcb.run(it));
+                .bodyToMono(responseType);
     }
 
     protected <T, R> Mono<R> post(
             @NonNull String uri,
             @NonNull T body,
             @NonNull ParameterizedTypeReference<R> responseType) {
-        ReactiveCircuitBreaker rcb = cbFactory.create("syncio-circuitbreaker");
         return buildClient().post()
                 .uri(uri)
                 .contentType(Objects.requireNonNull(MediaType.APPLICATION_JSON))
                 .bodyValue(body)
                 .retrieve()
-                .bodyToMono(responseType)
-                .transform(it -> rcb.run(it));
+                .bodyToMono(responseType);
     }
 
     protected WebClient buildClient() {
