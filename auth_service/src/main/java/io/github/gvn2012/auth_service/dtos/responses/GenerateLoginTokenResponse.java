88b4265446ca477dfe8ce839e421bbbdb1a83554
Gavin Ph <vietpham06262@gmail.com>
2026-04-17 20:36:14 +0700

    feat: implement cookie-based authentication for token management and hide tokens from JSON responses

--- auth_service/src/main/java/io/github/gvn2012/auth_service/dtos/responses/GenerateLoginTokenResponse.java
+++ auth_service/src/main/java/io/github/gvn2012/auth_service/dtos/responses/GenerateLoginTokenResponse.java
@@ -1,14 +1,19 @@
 package io.github.gvn2012.auth_service.dtos.responses;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GenerateLoginTokenResponse {
+    @JsonIgnore
     private String accessToken;
+    @JsonIgnore
     private String refreshToken;
     private List<String> userRoles;
 }
