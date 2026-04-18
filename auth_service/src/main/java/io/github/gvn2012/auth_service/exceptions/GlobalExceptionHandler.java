50c73db1cc4ce9f58786ee5f5b0c5baf392dc5c8
Gavin Ph <vietpham06262@gmail.com>
2026-04-18 08:02:31 +0700

    feat: improve error logging in global exception handlers and refine WebClient error handling in UserServiceImpl

--- auth_service/src/main/java/io/github/gvn2012/auth_service/exceptions/GlobalExceptionHandler.java
+++ auth_service/src/main/java/io/github/gvn2012/auth_service/exceptions/GlobalExceptionHandler.java
@@ -1,16 +1,20 @@
 package io.github.gvn2012.auth_service.exceptions;
 
 import io.github.gvn2012.auth_service.dtos.APIResource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<APIResource<?>> handleBaseException(BaseException e) {
+        log.error("BaseException in Auth Service: code={}, message={}, status={}", 
+            e.getCode(), e.getMessage(), e.getStatus());
         return ResponseEntity
                 .status(e.getStatus())
                 .body(
@@ -25,6 +29,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<APIResource<?>> handleException(Exception e) {
+        log.error("Unexpected error caught in Auth GlobalExceptionHandler: {}", e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(
--- permission_service/src/main/java/io/github/gvn2012/permission_service/exceptions/GlobalExceptionHandler.java
+++ permission_service/src/main/java/io/github/gvn2012/permission_service/exceptions/GlobalExceptionHandler.java
@@ -1,16 +1,19 @@
 package io.github.gvn2012.permission_service.exceptions;
 
 import io.github.gvn2012.permission_service.dtos.APIResource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
     public APIResource<?> handleBaseException(BaseException e) {
+        log.error("BaseException: code={}, message={}, status={}", e.getCode(), e.getMessage(), e.getStatus());
         return APIResource.error(
                 e.getCode(),
                 e.getMessage(),
@@ -64,6 +67,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<APIResource<?>> handleException(Exception e) {
+        log.error("Unexpected error caught in GlobalExceptionHandler: {}", e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(
