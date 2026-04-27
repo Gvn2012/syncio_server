8a5effd54c350af4b13f62ddd8aa788d9ab93afa
Gavin Ph <vietpham06262@gmail.com>
2026-04-27 10:21:24 +0700

    chore: comment out @EnableGrpcWebClient in ApiGatewayApplication

--- api_gateway/src/main/java/io/github/gvn2012/api_gateway/ApiGatewayApplication.java
+++ api_gateway/src/main/java/io/github/gvn2012/api_gateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package io.github.gvn2012.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @EnableGrpcWebClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
