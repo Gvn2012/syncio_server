a0f252237f69143d0a572154488ed8e80f6b8540
Gavin Ph <vietpham06262@gmail.com>
2026-05-01 16:40:13 +0700

    chore: add TODO comment for caching implementation in PostCommentServiceImpl

--- post_service/src/main/java/io/github/gvn2012/post_service/services/impls/PostCommentServiceImpl.java
+++ post_service/src/main/java/io/github/gvn2012/post_service/services/impls/PostCommentServiceImpl.java
@@ -29,6 +29,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
+// TODO: implement caching
 @Service
 @RequiredArgsConstructor
 public class PostCommentServiceImpl implements IPostCommentService {
